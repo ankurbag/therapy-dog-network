356e0f61e96849f1023a7fb7bc4085e83516df96
sabrishiyer <sabrish.iyer@gmail.com>
2017-04-29 10:55:55 -0400

    Setting up Framework

---
+++ Android/DogTherapy/app/src/main/java/com/tdn/therapydog/Activities/LoginActivity.java
@@ -0,0 +1,58 @@
+package com.tdn.therapydog.Activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+import com.tdn.therapydog.R;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        initViews();
+    }
+
+    private void initViews(){
+
+
+        ((Button)findViewById(R.id.humanButton)).setOnClickListener(this);
+        ((Button)findViewById(R.id.canineButton)).setOnClickListener(this);
+    }
+
+    private void startHumanActivity(){
+
+        startActivity(new Intent(this, HumanHomeActivity.class));
+    }
+
+    private void startCanineActivity(){
+
+        startActivity(new Intent(this, CanineHomeActivity.class));
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()){
+
+            case R.id.humanButton:
+                startHumanActivity();
+                break;
+
+            case R.id.canineButton:
+                startCanineActivity();
+                break;
+        }
+
+    }
+}
